40317148174baed66a2f49455493f5eb6e4457bb
cesartalves <cesartalvez@gmail.com>
2026-05-16 17:24:49 -0300

    Configure for various stocks

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/andybalholm/cascadia v1.3.3 // indirect
 	golang.org/x/net v0.54.0 // indirect
 	golang.org/x/text v0.37.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -4,24 +4,64 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"regexp"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"golang.org/x/net/html/charset"
-
-	"regexp"
+	"gopkg.in/yaml.v3"
+	"os"
 )
 
-var leadingQuestionMark = regexp.MustCompile(`^\?+`)
+type Config struct {
+	Papeis []string `yaml:"papeis"`
+}
+
+var weirdPrefix = regexp.MustCompile(`^[^\p{L}\p{N}]+`)
 
 func main() {
-	papel := "TAEE11"
+	config := loadConfig("papeis.yaml")
 
+	for _, papel := range config.Papeis {
+		dados, err := scrapePapel(papel)
+		if err != nil {
+			log.Printf("erro ao buscar %s: %v\n", papel, err)
+			continue
+		}
+
+		fmt.Println("==========")
+		fmt.Println("Papel:", papel)
+		fmt.Println("Cotação:", dados["Cotação"])
+		fmt.Println("P/L:", dados["P/L"])
+		fmt.Println("P/VP:", dados["P/VP"])
+		fmt.Println("Div. Yield:", dados["Div. Yield"])
+		fmt.Println("ROE:", dados["ROE"])
+		fmt.Println("Setor:", dados["Setor"])
+	}
+}
+
+func loadConfig(path string) Config {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var config Config
+
+	err = yaml.Unmarshal(content, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return config
+}
+
+func scrapePapel(papel string) (map[string]string, error) {
 	url := "https://fundamentus.com.br/detalhes.php?papel=" + papel
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	req.Header.Set("User-Agent", "Mozilla/5.0")
@@ -30,44 +70,37 @@ func main() {
 
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
-	// Corrige encoding ISO-8859-1 -> UTF-8
 	utf8Reader, err := charset.NewReader(resp.Body, resp.Header.Get("Content-Type"))
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	doc, err := goquery.NewDocumentFromReader(utf8Reader)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	dados := map[string]string{}
 
 	doc.Find("td.label").Each(func(i int, s *goquery.Selection) {
 		label := clean(s.Text())
-
 		value := clean(s.Next().Text())
 
 		dados[label] = value
 	})
 
-	for k, v := range dados {
-		fmt.Printf("%s => %s\n", k, v)
-	}
+	return dados, nil
 }
 
 func clean(s string) string {
 	s = strings.TrimSpace(s)
-
 	s = strings.ReplaceAll(s, "\n", "")
 	s = strings.ReplaceAll(s, "\t", "")
-
-	// remove ? no começo
-	s = leadingQuestionMark.ReplaceAllString(s, "")
+	s = weirdPrefix.ReplaceAllString(s, "")
 
 	return s
 }
